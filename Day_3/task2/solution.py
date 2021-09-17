280dc13d5556a1d3101b3efa957bc242fc7e36af
Atharva321 <atharvapatil19202@gmail.com>
2021-09-17 09:00:56 +0000

    regex raw string compile and finditer function

--- Day_3/task2/solution.py
+++ Day_3/task2/solution.py
@@ -6,4 +6,26 @@ how they evaluate.
 bad regex can take down your systems. 
 - Read about backtracking in REGEX evaluation.
 """
+import re 
+print("\tTab")
+
+#raw string interpret the string as a literal string 
+#so ita print \t also
+print(r"\tTab")
+#====================================================#
+#                   re.compile()                     #
+#====================================================#
+pattern=re.compile(r'Pa8ern')
+result_1 =pattern.findall('\tPa8ern: This is a Pa8ern')
+print(result_1)
+result_2=pattern.findall('Pa8ern:1 , Pa8ern:2, Pa8ern:3')
+print(result_2)
+
+#====================================================#
+#                   finditer()                     #
+#====================================================#
+found = pattern.finditer("Pa8ern")
+for finally_matched in found:
+    print(finally_matched)
+
 
